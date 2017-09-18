64144bf49c7d237e6f68ab61457c442e82e59142
SamWij <sam.wijesinha@gmail.com>
2017-09-18 19:26:41 +1000

    node server set up

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const pug = require('pug');
+
+
+// create our Express app
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views')); // this is the folder where we keep our pug files
+app.set('view engine', 'pug'); // we use the engine pug, mustache or EJS work great too
+
+// serves up static files from the public folder. Anything in public/ will just be served up as the file it is
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('port', process.env.PORT || 8080);
+const server = app.listen(app.get('port'), () => {
+  console.log(`Express running → PORT ${server.address().port}`);
+});
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Hey', message: 'Hello there!' });
+})
