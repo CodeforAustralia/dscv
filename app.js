ca07ddedad46647f940943ed3655e22e78f738b0
SamWij <sam.wijesinha@gmail.com>
2017-09-27 10:37:39 +1000

    added surveyjs

--- app.js
+++ app.js
@@ -7,19 +7,22 @@ const pug = require('pug');
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views')); // this is the folder where we keep our pug files
+app.set('views', path.join(__dirname, '/views')); // this is the folder where we keep our pug files
 app.set('view engine', 'pug'); // we use the engine pug, mustache or EJS work great too
 
 // serves up static files from the public folder. Anything in public/ will just be served up as the file it is
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
+
+
+
 
 app.set('port', process.env.PORT || 8080);
-const server = app.listen(app.get('port'), () => {
-  console.log(`Express running → PORT ${server.address().port}`);
+  const server = app.listen(app.get('port'), () => {
+    console.log(`Express running → PORT ${server.address().port}`);
 });
 
 app.get('/', (req, res) => {
-  res.render('index', { title: 'Hey', message: 'Hello there!' });
+  res.render('index', { title: 'Client Assessment Tool', message: 'Welcome to the Client Assessment Tool' });
 })
 
 module.exports = app;
