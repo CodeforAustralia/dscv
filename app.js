ae9289da16ff508dc0255dcbf37e8bf6b95e8baf
SamWij <sam.wijesinha@gmail.com>
2017-09-27 12:15:26 +1000

    updated server port to 3000

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.static(path.join(__dirname, '/public')));
 
 
 
-app.set('port', process.env.PORT || 8080);
+app.set('port', process.env.PORT || 3000);
   const server = app.listen(app.get('port'), () => {
     console.log(`Express running → PORT ${server.address().port}`);
 });
